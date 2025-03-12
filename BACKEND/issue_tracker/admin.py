a37dd72764099c8819d251df548350c1ef5b3e38
RugumayoA <rugumayoaisha@gmail.com>
2025-03-12 13:41:03 +0300

    registered models in admin.py

--- BACKEND/issue_tracker/admin.py
+++ BACKEND/issue_tracker/admin.py
@@ -1,7 +1,7 @@
 
 
 # Register your models here.
-rom django.contrib import admin
+from django.contrib import admin
 from .models import User, Issue, Comment, Notification, AuditTrail
 
 admin.site.register(User)
