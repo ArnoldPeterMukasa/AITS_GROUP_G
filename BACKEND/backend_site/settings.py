067dcd545fae12da6fda087acc4809ee26a0370b
RugumayoA <rugumayoaisha@gmail.com>
2025-03-14 10:43:06 +0300

    adding api.js and installing corsheaders for configuration

--- BACKEND/backend_site/settings.py
+++ BACKEND/backend_site/settings.py
@@ -34,6 +34,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'corsheaders',
     'rest_framework',
     'rest_framework_simplejwt',
     'rest_framework_simplejwt.token_blacklist',
@@ -63,6 +64,7 @@ SIMPLE_JWT = {
 AUTH_USER_MODEL = 'issue_tracker.User'
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -144,3 +146,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",  # React
+]
