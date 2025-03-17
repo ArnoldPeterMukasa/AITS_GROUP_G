1c0d1fdaf3f2607bfb09c4004d827d5b1dea6d44
Habibah65 <habibahndawula@gmail.com>
2025-03-17 18:00:41 +0300

    made another div container that displays a drop down menu to select your category

--- frontend_site/frontend/src/components/LoginPage.jsx
+++ frontend_site/frontend/src/components/LoginPage.jsx
@@ -16,4 +16,18 @@ function Login() {
                     <label htmlFor="password">Password:</label>
                     <input type="password" id="password" required />
                 </div>
+                <div className="form-group">
+                    <label htmlFor="role">Role:</label>
+                    <select id="role" required>
+                        <option value="">Select Role</option>
+                        <option value="student">Student</option>
+                        <option value="lecturer">Lecturer</option>
+                        <option value="registrar">Registrar</option>
+                    </select>
+                </div>
+                <button type="submit">Login</button>
+                <p>Don't have an account? <a href="">Signup</a></p>
+            </form>
+        </div>
+
 
