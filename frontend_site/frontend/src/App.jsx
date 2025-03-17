94a06d77a166a34278af2dc3fdafa300c34ffdb9
Habibah65 <habibahndawula@gmail.com>
2025-03-17 18:21:28 +0300

    added the login component and home component to the app.jsx

--- frontend_site/frontend/src/App.jsx
+++ frontend_site/frontend/src/App.jsx
@@ -1,8 +1,8 @@
-import Home from "./components/Home";
+import LoginPage from './COMPONENTS/LoginPage'
 function App() {
   return (
     <div>
-      <Home />
+      <LoginPage />
     </div>
   );
 }
