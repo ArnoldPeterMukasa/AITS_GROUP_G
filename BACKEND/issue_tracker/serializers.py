37fc4ecf07cd722aa430bba44d6d05e1159901f8
RugumayoA <rugumayoaisha@gmail.com>
2025-03-28 17:21:16 +0300

    debugged an attribute error

--- BACKEND/issue_tracker/serializers.py
+++ BACKEND/issue_tracker/serializers.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 from django.contrib.auth import get_user_model
 from .models import Issue, Comment, Notification, AuditTrail
 from rest_framework_simplejwt.tokens import RefreshToken
+from django.contrib.auth import authenticate
 
 User = get_user_model()
 
@@ -67,5 +68,6 @@ class LoginSerializer(serializers.Serializer):
         return {
             'refresh': str(refresh),
             'access': str(refresh.access_token),
-            'user': UserSerializer(user).data
+            'user_data': UserSerializer(user).data,
+            'user': user
         }
