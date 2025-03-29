5325bb823013e097502499c43d35bfc65f3e1c00
Habibah65 <habibahndawula@gmail.com>
2025-03-29 11:09:34 +0300

    removed the academic registar component cause the outlet hook can render all child components

--- FRONTEND/src/App.jsx
+++ FRONTEND/src/App.jsx
@@ -1,12 +1,12 @@
 import { Outlet } from "react-router-dom";
-import AcademicRegistrarDashboard from "./components/AcademicRegistrarDashboard";
+
 
 function App() {
   return (
     <>
         <div>
       <Outlet />
-      <AcademicRegistrarDashboard />
+      
         </div>
     </>
     
