5a54783cc9b56593141e39aeaabe7a5e98d25796
AKELLO SUNDAY OBONYO <sundaysierra61@gmail.com>
2025-03-30 16:08:40 +0100

    Added new files LecturerDashboard.jsx and LecturerDashboard.css

--- FRONTEND/src/main.jsx
+++ FRONTEND/src/main.jsx
@@ -9,7 +9,7 @@ import LoginPage from './components/LoginPage.jsx';
 import Register from './components/Register.jsx';
 import StudentDashboard from './components/StudentDashboard.jsx';
 import AcademicRegistrarDashboard from './components/AcademicRegistrarDashboard.jsx';
-
+import LecturerDashboard from './components/LecturerDashboard.jsx';
 const root=ReactDOM.createRoot(document.getElementById('root'));root.render(
   <React.StrictMode>
     <Router>
@@ -20,6 +20,7 @@ const root=ReactDOM.createRoot(document.getElementById('root'));root.render(
         <Route path="/app" element={<App/>} />
         <Route path="/StudentDashboard" element={<StudentDashboard/>}/>
         <Route path = "/AcademicRegistrarDashboard" element={<AcademicRegistrarDashboard/>}/>
+        <Route path="/LecturerDashboard" element={<LecturerDashboard/>}/>
 
       </Routes>
     </Router>
