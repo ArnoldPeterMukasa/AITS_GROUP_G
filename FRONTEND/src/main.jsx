62dbcd00feb5affbf9bb13fa310d783180759b71
AKELLO SUNDAY OBONYO <sundaysierra61@gmail.com>
2025-03-31 17:18:57 +0100

    added a navbar in the lecturer dashboard

--- FRONTEND/src/main.jsx
+++ FRONTEND/src/main.jsx
@@ -10,6 +10,7 @@ import Register from './components/Register.jsx';
 import StudentDashboard from './components/StudentDashboard.jsx';
 import AcademicRegistrarDashboard from './components/AcademicRegistrarDashboard.jsx';
 import LecturerDashboard from './components/LecturerDashboard.jsx';
+
 const root=ReactDOM.createRoot(document.getElementById('root'));root.render(
   <React.StrictMode>
     <Router>
