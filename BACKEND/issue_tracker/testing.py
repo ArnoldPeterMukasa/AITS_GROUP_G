49c3d326ec48a9013c36ebda71b23f7e0a3b7297
RugumayoA <rugumayoaisha@gmail.com>
2025-03-31 22:16:16 +0300

    creatied a testing file to reuse our values for future use or reference

---
+++ BACKEND/issue_tracker/testing.py
@@ -0,0 +1,25 @@
+#for the registering a student
+{
+  "username": "aisha_student3",
+  "email": "aisha3@example.com",
+  "password": "strongpassword123",
+  "user_type": "student",
+  "department": "Computer Science"
+}
+
+
+#for loging in a student
+{
+  "username": "aisha_student3",
+  "password": "strongpassword123"
+}
+
+
+# for creating an issue
+{
+  "title": "Missing Python Marks",
+  "description": "I submitted all coursework but marks are missing.",
+  "category": "missing_marks"
+}
+
+
