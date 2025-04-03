4d2e515e4618ac49f3fe60a4b6deeb6dcb095173
ArnoldPeterMukasa <info.ap.mukasa@gmail.com>
2025-04-03 14:58:16 +0300

    modifications with imports in views.py

--- BACKEND/issue_tracker/views.py
+++ BACKEND/issue_tracker/views.py
@@ -3,7 +3,6 @@ from rest_framework import generics, permissions,status,serializers
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from django.contrib.auth import get_user_model
-from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from .models import Issue, Notification
 from .serializers import UserSerializer, RegisterSerializer, LoginSerializer, IssueSerializer, NotificationSerializer
@@ -13,6 +12,9 @@ from datetime import timedelta, datetime
 from django.core.mail import send_mail
 from django.contrib.auth import authenticate
 from django.conf import settings
+from django.contrib.auth.tokens import default_token_generator
+from django.utils.encoding import force_bytes
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
 
 User = get_user_model()
 
