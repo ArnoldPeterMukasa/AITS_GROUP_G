363384bea6fc0b35ea92f21b37a2eb3ea475883a
Habibah65 <habibahndawula@gmail.com>
2025-04-15 20:26:35 +0300

    modified

--- FRONTEND/src/components/Register.jsx
+++ FRONTEND/src/components/Register.jsx
@@ -65,6 +65,7 @@ function RegisterPage() {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json",
+                   
                 },
                 body: JSON.stringify(requestBody),
             });
