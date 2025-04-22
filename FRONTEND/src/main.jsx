a6cb5710016d43be6819f0a89a9cef610da9fe7d
Habibah65 <habibahndawula@gmail.com>
2025-04-22 20:11:25 +0300

    modified

--- FRONTEND/src/main.jsx
+++ FRONTEND/src/main.jsx
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './index.css';
 import App from './App.jsx';
-import Home from './components/Home.jsx';t
+import Home from './components/Home.jsx';
 import LoginPage from './components/LoginPage.jsx';
 import Register from './components/Register.jsx';
 import StudentDashboard from './components/StudentDashboard.jsx';
