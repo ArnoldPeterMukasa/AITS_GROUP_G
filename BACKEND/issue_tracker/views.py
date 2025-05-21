a89aa0a3ed660da1959e263eca452367ab3bb926
RugumayoA <rugumayoaisha@gmail.com>
2025-05-21 13:41:45 +0300

    moified

--- BACKEND/issue_tracker/views.py
+++ BACKEND/issue_tracker/views.py
@@ -885,11 +885,4 @@ class AssignedIssuesView(APIView):
 
 
 
-        #commit
-        #commit 2
-        #commit 3
-        #commit 4
-        #commit 5
-        #commit 6
-        #commit7
-        #commit 8
+        
