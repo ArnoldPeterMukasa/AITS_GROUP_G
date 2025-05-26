43ab8250c3fd7f977998f754df8275adad8d331a
RugumayoA <rugumayoaisha@gmail.com>
2025-05-26 17:58:32 +0300

    commit

--- BACKEND/issue_tracker/tests.py
+++ BACKEND/issue_tracker/tests.py
@@ -30,15 +30,3 @@
 }
 
 
-# {aisha_student3: "strongpassword123"}
-#{arnold_student1: "strongpassword123"}
-#{joshua_student2: "strongpassword123"}
-#{karungi_student4: "strongpassword123"}
-#{aisha_student5: "strongpassword123"}
-#{karungi_student6: "strongpassword123"}
-#{habibah_student7: "strongpassword123"}
-#{mehya_student8: "strongpassword123"}
-#{rugumayo_student9: "strongpassword123"}
-#{muhwezi_student10: "strongpassword123"}
-#{  "arnold_student1": "strongpassword123"}
-#{  "joshua_student2": "strongpassword123"}
