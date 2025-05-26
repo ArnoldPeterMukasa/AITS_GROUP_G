455216ed136cc0e166f3bd178c79470a945dc270
RugumayoA <rugumayoaisha@gmail.com>
2025-05-26 17:14:22 +0300

    modified

--- BACKEND/issue_tracker/tests.py
+++ BACKEND/issue_tracker/tests.py
@@ -37,3 +37,5 @@
 #{aisha_student5: "strongpassword123"}
 #{karungi_student6: "strongpassword123"}
 #{habibah_student7: "strongpassword123"}
+#{mehya_student8: "strongpassword123"}
+#{rugumayo_student9: "strongpassword123"}
